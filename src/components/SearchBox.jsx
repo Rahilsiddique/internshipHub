fa83d6c0ab344a1047fb6b713798f3b33d3e3119
Rahilsiddique <siddiquerahil19@gmail.com>
2022-12-14 22:36:43 +0530

    added faker js and removed search button
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,8 +4,10 @@ const SearchBox = () => {
   return (
     <div className="py-4 px-6">
       <div className="bg-slate-200 flex justify-center items-center h-24 rounded-lg gap-3">
-        <input className="w-[60%] h-8 rounded-lg outline-blue-400 p-1" />
-        <button className="bg-blue-400 px-3 py-1 rounded-lg">Search</button>
+        <input
+          className="w-[60%] h-8 rounded-lg outline-blue-400 p-1"
+          placeholder="Search by skills"
+        />
       </div>
       <div></div>
     </div>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,7 +1,15 @@
 import React, { createContext } from "react";
+import { faker } from "@faker-js/faker";
 
 const GlobalContext = createContext();
-
+const internships = [...Array(30)].map(() => ({
+  id: faker.datatype.uuid(),
+  company: faker.company.companyName(),
+  stipend: faker.finance.amount(5, 100, 0, "₹"),
+  duration: faker.datatype.number({ min: 1, max: 12 }),
+  position: faker.datatype.number({ min: 1, max: 20 })
+}));
+console.log(internships);
 const Context = ({ children }) => {
   return <GlobalContext.Provider>{children}</GlobalContext.Provider>;
 };
